039a83fb703b10b601faaf544cff2df56b081714
daboggg <daboggg@ya.ru>
2019-08-09 17:42:10 +0300

    end video 4

--- src/main/java/ru/zinin/sarafan/controller/MessageController.java
+++ src/main/java/ru/zinin/sarafan/controller/MessageController.java
@@ -1,10 +1,13 @@
 package ru.zinin.sarafan.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.zinin.sarafan.domain.Message;
+import ru.zinin.sarafan.domain.Views;
 import ru.zinin.sarafan.repo.MessageRepo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -19,18 +22,20 @@ public class MessageController {
     }
 
     @GetMapping
+    @JsonView(Views.IdName.class)
     public List<Message> list() {
         return messageRepo.findAll();
     }
 
     @GetMapping("{id}")
+    @JsonView(Views.FullMessage.class)
     public Message getOne(@PathVariable("id") Message message) {
         return message;
     }
 
     @PostMapping
     public Message create(@RequestBody Message message) {
-
+        message.setCreationDate(LocalDateTime.now());
         return messageRepo.save(message);
     }
 
--- src/main/java/ru/zinin/sarafan/domain/Message.java
+++ src/main/java/ru/zinin/sarafan/domain/Message.java
@@ -1,10 +1,13 @@
 package ru.zinin.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -14,8 +17,16 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)
     private Long id;
+
+    @JsonView(Views.IdName.class)
     private String text;
+
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
 
     public Long getId() {
         return id;
@@ -31,5 +42,13 @@ public class Message {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 }
